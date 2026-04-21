fada7d9a00e48de3f32d44ac09ff8c206910c8d8
Zeineb Sioud <yy3.zeinebsioud.08@gmail.com>
2026-04-21 16:49:09 +0100

    Ajouter la multi dans App.java

--- App.java
+++ App.java
@@ -3,6 +3,7 @@ public class App {
 		int a = Integer.parseInt(args[0].trim());
 		int b = Integer.parseInt(args[1].trim());
 		System.out.println("Somme = " + (a + b)); 	
+		System.out.println("Produit = " + (a * b))
 	}
 
 }

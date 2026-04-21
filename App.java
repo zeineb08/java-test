27067b518eea524476b1656dabe1486336539042
Zeineb Sioud <yy3.zeinebsioud.08@gmail.com>
2026-04-21 14:40:35 +0100

    supprission des espaces dans les arguments

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App {
 	public static void main(String[] args) {
-		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);
+		int a = Integer.parseInt(args[0].trim());
+		int b = Integer.parseInt(args[1].trim());
 		System.out.println("Somme = " + (a + b)); 	
 	}
 
